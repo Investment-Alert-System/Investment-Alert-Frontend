a2a08be636de296650ae8dcbf2949bcdb4fbf83b
Philipp Pollmann <philipp@geuer-pollmann.de>
2024-04-20 20:42:17 +0200

    feat(project list): a list that includes all projects for the investment-alert-system

--- src/services/ProjectList.ts
+++ src/services/ProjectList.ts
@@ -1,5 +1,14 @@
-//import Project from "../model/Project.ts";
+import {Project} from "../model/Project.ts";
 
-/*function getAllProjects(): Array<Project>{
-    return Array<Project("")>
-}*/
+export function getAllProjects(){
+    return [ // List of all projects
+        new Project(
+            "Traefik",
+            "Traefik is a modern HTTP reverse proxy and load balancer that makes deploying microservices easy.",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1b/Traefik.logo.png",
+            "traefik.localhost",
+            "https://traefik.io/",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1b/Traefik.logo.png"
+        ),
+    ]
+}
