1d9c67b4a6e6ca3efa444f4c00730135c649dfc1
Philipp Pollmann <philipp@geuer-pollmann.de>
2024-04-20 21:57:07 +0200

    feat(testing): added testing pipeline with report

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,3 @@
-// jest.config.mjs
 export default {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
@@ -8,5 +7,12 @@ export default {
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy"
     },
-    setupFilesAfterEnv: ['./src/tests/setupTests.ts']
+    setupFilesAfterEnv: ['./src/tests/setupTests.ts'],
+    collectCoverage: true,  // Aktiviert die Erfassung von Testabdeckung
+    coverageDirectory: 'coverage',  // Speichert Abdeckungsberichte im Verzeichnis "coverage"
+    collectCoverageFrom: [  // Gibt an, von welchen Dateien die Abdeckung erfasst werden soll
+        "src/**/*.{ts,tsx}",
+        "!src/**/*.d.ts",
+        "!src/index.tsx",  // Schließe spezifische Dateien nach Bedarf aus
+    ]
 };
