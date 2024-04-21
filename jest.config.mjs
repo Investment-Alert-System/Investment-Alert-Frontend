fdf7a05bb9f898316d05b7ee9389c879e08954b9
Philipp Pollmann <philipp@geuer-pollmann.de>
2024-04-21 17:04:26 +0200

    feat(testing): added example test

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,11 +8,12 @@ export default {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy"
     },
     setupFilesAfterEnv: ['./src/tests/setupTests.ts'],
-    collectCoverage: true,  // Aktiviert die Erfassung von Testabdeckung
-    coverageDirectory: 'coverage',  // Speichert Abdeckungsberichte im Verzeichnis "coverage"
-    collectCoverageFrom: [  // Gibt an, von welchen Dateien die Abdeckung erfasst werden soll
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
         "src/**/*.{ts,tsx}",
         "!src/**/*.d.ts",
-        "!src/index.tsx",  // Schließe spezifische Dateien nach Bedarf aus
-    ]
+        "!src/index.tsx",
+    ],
+    testMatch: ["**/tests/**/*.test.ts"]
 };
---
+++ src/tests/api.test.ts
@@ -0,0 +1,6 @@
+describe('Tests for all api calls', () => {
+
+    test('addiert 1 + 2 um 3 zu ergeben', () => {
+        expect(3).toBe(3);
+    });
+});
